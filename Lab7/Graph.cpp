718195fcfcbf404db77f5397ef28c34d66f11e6f
carnieson <aidangmurphree@gmail.com>
2021-11-14 01:05:37 -0800

    finished lab 7

--- Lab7/Graph.cpp
+++ Lab7/Graph.cpp
@@ -9,32 +9,124 @@
 #include <limits.h>
 using namespace std;
 
+//default constructor
 Graph::Graph()
 {
+	nodes = {};
 }
 
+
+//prints all the neighbors of all the nodes in a tree. Simply iterates through all the nodes and prints their neighbors
 void Graph::printAdjList()
 {
+	for (int i = 0; i < nodes.size(); i++)
+	{
+		Node currentNode = nodes[i];
+		cout << currentNode.id << ": ";
+		for (int j = 0; j < currentNode.neighbors.size(); j++)
+		{
+			cout << currentNode.neighbors[j]->id << " ";
+		}
+		cout << endl;
+	}
 }
 
+//checks if node v is a neighbor of u. Iterates through u's neighbors to see if one of their IDs matches v. 
 bool Graph::isNeighbor(int u, int v)
 {
-  return false;
+	for (int i=0; i<nodes[u].neighbors.size(); i++)
+	{
+		if (nodes[u].neighbors[i]->id == v) return true;
+	}
+	return false;
 }
 
+
+//prepares the graph for depth first search by setting the nodes' properties, then calls the recursive DFSVisit on each of the nodes (in chase of disconnected graphs)
 void Graph::DFS()
 {
+	for (int i=0; i < nodes.size(); i++)
+	{
+		nodes[i].visited = false;			//sets a baseline for all the nodes in the graph
+		nodes[i].predecessor = nullptr;
+		nodes[i].discovered = -1;
+		nodes[i].finished = -1;
+	}
+
+	int time = 0;
+	for (int j=0; j<nodes.size(); j++)			//ensures that all nodes in the graph are visited, even if they're disconnected
+	{
+		if (nodes[j].visited == false)
+		{
+			time = DFSVisit(j, time);			//recursively visits all the neighbors of the node, starting with itself
+		}
+	}
 }
 
+//recursively visits all of the neighbors of a given node, updating the nodes with time of discovery and when all their neighbors have visited
 int Graph::DFSVisit(int s, int time)
-{
-  return time;
+{	
+	time++;
+	nodes[s].discovered = time;			//set discovered time
+	nodes[s].visited = true;
+
+	for (int i=0; i < nodes[s].neighbors.size(); i++)				//visit all the neighbors
+	{
+		if (nodes[s].neighbors[i]->visited == false)				//if the node hasn't been visited before, explore it further before continueing with this node
+		{
+			nodes[s].neighbors[i]->predecessor = &nodes[s];
+			time = DFSVisit( nodes[s].neighbors[i]->id , time);
+		}
+	}
+	time++;
+	nodes[s].finished = time;				//after visiting all the neighbors and subneighbors, mark finished time
+	return time;							//return time so it can be used for other subgraphs
 }
 
+
+//breadth first search, visiting all of a given node's neighbors before continueing on to their neighbors. Updates each node's distance from the starting point
 void Graph::BFS(int s)
 {
+	for (int i=0; i<nodes.size(); i++)		//set all nodes to a baseline
+	{
+		nodes[i].dist = INT_MAX;
+		nodes[i].visited = false;
+		nodes[i].predecessor = nullptr;
+	}
+
+	nodes[s].dist = 0;						//starting node has distance of 0
+	nodes[s].visited = true;
+
+	vector<Node*> queue = {};
+	queue.push_back(&nodes[s]);				//queue holds the list of nodes whose neighbors need to be explore 
+
+	while (queue.size() > 0)				//visit all nodes
+	{
+		Node* currentNode = queue[0];		
+		queue.erase(queue.begin());
+		for (int j=0; j < currentNode->neighbors.size(); j++)		//visit all of this node's neighbors
+		{
+			if(currentNode->neighbors[j]->visited == false)
+			{
+				currentNode->neighbors[j]->dist = currentNode->dist + 1;			//set the neighbor's distance
+				currentNode->neighbors[j]->visited = true;
+				currentNode->neighbors[j]->predecessor = currentNode;
+				queue.push_back(currentNode->neighbors[j]);							//add it to this list of nodes whose neighbors to explore
+			}
+		}
+	}
 }
 
+//uses breadth first search to calculate the distance of each node from the given starting node, then adds those distances to a vector
 vector<int> Graph::distancesFrom(int s)
 {
+	BFS(s);					//sets the distances of the graph
+
+	vector<int> distances = {};
+
+	for (int i=0; i < nodes.size(); i++)		//adds these distances to a vector, in the order of the nodes in the graph nodes vector
+	{
+		distances.push_back(nodes[i].dist);
+	}
+	return distances;
 }
--- Lab7/GraphDriver.cpp
+++ Lab7/GraphDriver.cpp
@@ -13,48 +13,52 @@ using namespace std;
 Graph makeGraph();
 void checkNeighbors(Graph &G);
 void checkDistancesFrom(Graph &G);
-int main(){
-  Graph G;
+int main()
+{
+	Graph G;
 
-  int operation = -1;
-  cin >> operation;
+	int operation = -1;
+	cin >> operation;
 
-  while (operation > 0){
-    switch(operation){
-      case 1:
-        cout << "MAKE GRAPH" << endl;
-        G = makeGraph();
-        break;
-      case 2:
-        cout << "ADJACENCY LIST" << endl;
-        G.printAdjList();
-        break;
-      case 3:
-        cout << "IS NEIGHBOR" << endl;
-        checkNeighbors(G);
-        break;
-      case 4:
-        cout << "DFS" << endl;
-        G.DFS();
-        for (int i = 0; i < G.nodes.size(); i++){
-          cout << G.nodes[i].id << ": " 
-               << (G.nodes[i].visited ? "True" : "False") << ", "
-               << G.nodes[i].discovered << ", "
-               << G.nodes[i].finished << ", "
-               << ((G.nodes[i].predecessor != nullptr) ? G.nodes[i].predecessor->id : -1) << endl;
-        }
-        break;
-      case 5:
-        cout << "DISTANCES FROM" << endl;
-        checkDistancesFrom(G);
-        break;
-      default:
-        break;
-    }
-    cin >> operation;
-  }
+	while (operation > 0)
+	{
+		switch (operation)
+		{
+		case 1:
+			cout << "MAKE GRAPH" << endl;
+			G = makeGraph();
+			break;
+		case 2:
+			cout << "ADJACENCY LIST" << endl;
+			G.printAdjList();
+			break;
+		case 3:
+			cout << "IS NEIGHBOR" << endl;
+			checkNeighbors(G);
+			break;
+		case 4:
+			cout << "DFS" << endl;
+			G.DFS();
+			for (int i = 0; i < G.nodes.size(); i++)
+			{
+				cout << G.nodes[i].id << ": "
+					 << (G.nodes[i].visited ? "True" : "False") << ", "
+					 << G.nodes[i].discovered << ", "
+					 << G.nodes[i].finished << ", "
+					 << ((G.nodes[i].predecessor != nullptr) ? G.nodes[i].predecessor->id : -1) << endl;
+			}
+			break;
+		case 5:
+			cout << "DISTANCES FROM" << endl;
+			checkDistancesFrom(G);
+			break;
+		default:
+			break;
+		}
+		cin >> operation;
+	}
 
-  return 0;
+	return 0;
 }
 
 /*******************************************************************************************************************
@@ -63,54 +67,62 @@ int main(){
  * each contain a pair of space separated integers representing edges in the graph.                                *
  * return - Graph - a graph constructed based on the user input                                                    *
  * *****************************************************************************************************************/
-Graph makeGraph(){
-  int n = 0, m = 0;
-  cin >> n >> m;
-  Graph G;
-  for (int i = 0; i < n; i++){
-    Node v(i);
-    G.nodes.push_back(v);
-  }
-  for (int i = 0; i < m; i++){
-    int u = -1, v = -1;
-    cin >> u >> v;
-    G.nodes[u].neighbors.push_back(&G.nodes[v]);
-    G.nodes[v].neighbors.push_back(&G.nodes[u]);
-  }
-  return G;
+Graph makeGraph()
+{
+	int n = 0, m = 0;
+	cin >> n >> m;
+	Graph G;
+	for (int i = 0; i < n; i++)
+	{
+		Node v(i);
+		G.nodes.push_back(v);
+	}
+	for (int i = 0; i < m; i++)
+	{
+		int u = -1, v = -1;
+		cin >> u >> v;
+		G.nodes[u].neighbors.push_back(&G.nodes[v]);
+		G.nodes[v].neighbors.push_back(&G.nodes[u]);
+	}
+	return G;
 }
 
 /**************************************************************************************************************
  * A function to collect input from the user for checking whether or not nodes are neighbors in a given graph *
  * G - Graph & - a graph in which to check neighbors for a set of pairs of nodes                              *
  * ************************************************************************************************************/
-void checkNeighbors(Graph &G){
-  int num = -1;
-  cin >> num;
-  for (int i = 0; i < num; i++){
-    int u = -1, v = -1;
-    cin >> u >> v;
-    cout << u << " " << v << " " << (G.isNeighbor(u, v) ? "True": "False") << endl;
-  }
+void checkNeighbors(Graph &G)
+{
+	int num = -1;
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		int u = -1, v = -1;
+		cin >> u >> v;
+		cout << u << " " << v << " " << (G.isNeighbor(u, v) ? "True" : "False") << endl;
+	}
 }
 
 /*****************************************************************************************************************
  * A function to collect input from the user for checking distances from one node to all others in a given graph *
  * G - Graph & - a graph in which to check distances for a set of nodes acting as the source                     *
  * ***************************************************************************************************************/
-void checkDistancesFrom(Graph &G){
-  int num = -1;
-  cin >> num;
-  for (int i = 0; i < num; i++){
-    int s = -1;
-    cin >> s;
-    vector<int> dists = G.distancesFrom(s);
-    cout << "source " << s << ": ";
-    for (int j = 0; j < G.nodes.size(); j++){
-      cout << "(to " << G.nodes[j].id << ": " 
-           << ((G.nodes[j].dist == INT_MAX) ? -1 : G.nodes[j].dist) << ", "
-           << ((dists[j] == INT_MAX) ? -1 : dists[j]) << ") ";
-    }
-    cout << endl;
-  }
+void checkDistancesFrom(Graph &G)
+{
+	int num = -1;
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		int s = -1;
+		cin >> s;
+		vector<int> dists = G.distancesFrom(s);
+		cout << "source " << s << ": ";
+		for (int j = 0; j < G.nodes.size(); j++)
+		{
+			cout << "(to " << G.nodes[j].id << ": "
+				 << ((G.nodes[j].dist == INT_MAX) ? -1 : G.nodes[j].dist) << ", "
+				 << ((dists[j] == INT_MAX) ? -1 : dists[j]) << ") ";
+		}
+		cout << endl;
+	}
 }
